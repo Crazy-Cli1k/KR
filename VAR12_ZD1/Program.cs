6e59d4c2d26b8ecaca8efd273452383cccbbe6b1
dimas <dimas@DESKTOP-GPBN6TO>
2021-10-25 12:03:22 +0500

    Добавьте файлы проекта.

---
+++ VAR12_ZD1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Collections;
+
+namespace VAR12_ZD1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string zd = "Создать текстовый файл.\nВывести на экран гласные буквы этого файла в обратном порядке.";
+
+            string path = Directory.GetCurrentDirectory();
+            FileStream fileStream = null;
+
+            // проверяем существует ли файл файл
+            if (!File.Exists(path + "/t.txt"))
+            {
+                fileStream = File.Create(path + "/t.txt"); // создаем файл
+                StreamWriter sr1 = new StreamWriter(fileStream);
+                sr1.Write(zd);
+                sr1.Close();
+            }
+
+                fileStream = File.Open(path + "/t.txt", FileMode.Open);
+
+                StreamReader sr = new StreamReader(fileStream);
+                string s = sr.ReadToEnd();
+                sr.Close();
+
+                Stack st = new Stack();
+                string abc = "аоэеиыуёюя";
+
+                for (int i = 0; i < s.Length; i++)
+                {
+                    if ((abc.Contains(s[i])) || (abc.ToUpper().Contains(s[i]))) st.Push(s[i]);
+                }
+                //выводим
+                Console.WriteLine("Глассные буквы из файла в обратном порядке");
+                while (st.Count > 0)
+                {
+                    Console.Write($"{st.Pop()} ");
+                }
+
+        }
+    }
+}
---
+++ VAR12_ZD2/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace VAR12_ZD2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            if (File.Exists("t.txt"))
+            {
+                int n1 = 0, n2 = 0, n3 = 0;
+                Console.WriteLine("Введите начало интервала(а):");
+              if (Int32.TryParse(Console.ReadLine(), out int a))
+              {
+                Console.WriteLine("Введите начало интервала(b):");
+                if (Int32.TryParse(Console.ReadLine(), out int b))
+                {
+                    if (a < b)
+                    {
+                        Queue<int> fromAToB = new Queue<int>();
+                        Queue<int> lessThenA = new Queue<int>();
+                        Queue<int> moreThenB = new Queue<int>();
+                        StreamReader sr = new StreamReader("t.txt");
+                        List<string> num1 = new List<string>();
+
+                        while (!sr.EndOfStream)
+                        {
+                            num1.AddRange(sr.ReadLine().Split(" "));
+                        }
+                        sr.Close();
+
+                        for (int i = 0; i < num1.Count; i++)
+                        {
+                            int number = int.Parse(num1[i]);
+                            if ((number >= a) && (number <= b))
+                            {
+                                fromAToB.Enqueue(number);
+                                n1++;
+                            }
+
+                            else if (number < a)
+                            {
+                                lessThenA.Enqueue(number);
+                                n2++;
+                            }
+
+                            else
+                            {
+                                moreThenB.Enqueue(number);
+                                n3++;
+                            }
+
+                        }
+
+                        for (int i = 0; i < n1; i++)
+                        {
+                            Console.Write(fromAToB.Dequeue() + " ");
+                        }
+                        Console.WriteLine();
+                        for (int j = 0; j < n2; j++)
+                        {
+                            Console.Write(lessThenA.Dequeue() + " ");
+                        }
+                        Console.WriteLine();
+                        for (int k = 0; k < n3; k++)
+                        {
+                            Console.Write(moreThenB.Dequeue() + " ");
+                        }
+
+                    }else { Console.WriteLine("а не должно быть больше b!"); }
+
+                }else
+                 { Console.WriteLine("Файл не найден!"); }
+               }
+            }
+        }
+    }
+}
